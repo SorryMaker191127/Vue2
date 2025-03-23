8c1ccafbfc7098fc20cb2cf3b3e7290b0aefc46a
SorryMaker_Mac <Sorrymaker191127@outlook.com>
2025-03-23 15:29:24 +0800

    组件自定义事件

---
+++ vue2_project/05.组件自定义事件/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- 在子组件标签中绑定自定义事件 -->
+    <HelloWorld @custom-event="customEventFunction" />
+    <!-- 自定义事件也能添加事件修饰符 -->
+    <HelloWorld @custom-event.once="customEventFunction" />
+    <!-- 通过ref绑定自定义事件 -->
+    <HelloWorld ref="helloWorld" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    // 自定义事件的处理函数
+    customEventFunction(props) {
+      console.log("自定义事件被触发了! " + props);
+    },
+  },
+  mounted() {
+    // 通过ref绑定自定义事件
+    this.$refs.helloWorld.$on("custom-event", this.customEventFunction);
+    // 或者绑定带修饰符事件
+    //this.$refs.helloWorld.$once("custom-event", this.customEventFunction);
+  },
+};
+</script>
+
+<style></style>
---
+++ vue2_project/05.组件自定义事件/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="helloWorld">
+    <button @click="triggerEvent">触发自定义事件</button>
+    <br />
+    <button @click="unbindEvent">解绑自定义事件</button>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    triggerEvent() {
+      // 触发自定义事件
+      this.$emit("custom-event", "Hello World");
+    },
+    unbindEvent() {
+      // 解绑单个事件
+      this.$off("custom-event");
+      // 解绑多个事件
+      // this.$off(["custom-event1", "custom-event2"]);
+      // 解绑所有事件
+      // this.$off();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
